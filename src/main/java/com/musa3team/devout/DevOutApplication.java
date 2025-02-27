9aa0c66941216d6b92e3a3a084b63cc26d6e2b80
jyedev04 <jyedev04@gmail.com>
2025-02-28 01:35:02 +0900

    first commit

---
+++ src/main/java/com/musa3team/devout/DevOutApplication.java
@@ -0,0 +1,13 @@
+package com.musa3team.devout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevOutApplication.class, args);
+    }
+
+}
