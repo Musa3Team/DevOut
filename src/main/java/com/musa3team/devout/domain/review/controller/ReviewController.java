3180862b9b53f628773cfae91f45e41127cb7cb3
DestroTHS <osk3130@gmail.com>
2025-03-04 20:44:22 +0900

    feat : add

--- src/main/java/com/musa3team/devout/domain/review/controller/ReviewController.java
+++ src/main/java/com/musa3team/devout/domain/review/controller/ReviewController.java
@@ -1,4 +1,24 @@
 package com.musa3team.devout.domain.review.controller;
 
+import com.musa3team.devout.domain.review.repository.ReviewRepository;
+import com.musa3team.devout.domain.review.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/review")
+@RequiredArgsConstructor
 public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    @PostMapping
+    public ResponseEntity<ReviewResponseDto> save(@RequestBody CreateReviewRequestDto requestDto) {
+
+        ReviewResponseDto reviewResponseDto = reviewService.save();
+    }
 }
--- src/main/java/com/musa3team/devout/domain/review/service/ReviewService.java
+++ src/main/java/com/musa3team/devout/domain/review/service/ReviewService.java
@@ -1,4 +1,7 @@
 package com.musa3team.devout.domain.review.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ReviewService {
 }
