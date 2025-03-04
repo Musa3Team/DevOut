a528d23ef653879f37c7031bf720c2355d74099d
jyedev04 <jyedev04@gmail.com>
2025-03-04 19:45:57 +0900

    feat : add create order 주문 추가 기능 생성 추후 테스트 데이터 제거 필요

--- src/main/java/com/musa3team/devout/domain/member/repository/MemberRepository.java
+++ src/main/java/com/musa3team/devout/domain/member/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package com.musa3team.devout.domain.member.repository;
 import com.musa3team.devout.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByIdAndMemberRole(Long id, String userRole);
 }
--- src/main/java/com/musa3team/devout/domain/store/repository/StoreRepository.java
+++ src/main/java/com/musa3team/devout/domain/store/repository/StoreRepository.java
@@ -1,4 +1,11 @@
 package com.musa3team.devout.domain.store.repository;
 
-public interface StoreRepository {
+import com.musa3team.devout.domain.store.entity.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalTime;
+import java.util.Optional;
+
+public interface StoreRepository extends JpaRepository<Store, Long> {
+    Optional<Store> findByIdAndOpenTimeBeforeAndCloseTimeAfterAndStatus(Long id, LocalTime before, LocalTime after, String status);
 }
