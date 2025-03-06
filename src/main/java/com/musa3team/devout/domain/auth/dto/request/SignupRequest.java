7ac96bd9ef9f109839b72558c2834fab8a46de8b
jyedev04 <jyedev04@gmail.com>
2025-03-06 17:21:45 +0900

    feat : swagger 테스트를 위한 example 추가

--- src/main/java/com/musa3team/devout/domain/auth/dto/request/SignupRequest.java
+++ src/main/java/com/musa3team/devout/domain/auth/dto/request/SignupRequest.java
@@ -1,6 +1,7 @@
 package com.musa3team.devout.domain.auth.dto.request;
 
 import com.musa3team.devout.domain.member.entity.MemberRole;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
@@ -10,20 +11,24 @@ public class SignupRequest {
 
     private String name;
 
+    @Schema(description = "이메일", example = "email@email.com")
     @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식이 올바르지 않습니다.")
     @NotBlank(message = "이메일은 필수 입력 값입니다.")
     private String email;
 
+    @Schema(description = "비밀번호", example = "test123!!")
     @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대소문자, 숫자, 특수문자를 사용하세요.")
     @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
     private String password;
 
     private String address;
 
+    @Schema(description = "전화번호", example = "010-1234-1234")
     @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "전화번호 형식이 올바르지 않습니다.")
     @NotBlank(message = "전화번호는 필수 입력 값입니다.")
     private String phoneNumber;
 
+    @Schema(description = "유저상태", example = "CUSTOMER")
     private MemberRole memberRole;
 
 
--- src/main/java/com/musa3team/devout/domain/store/dto/StoreRequestDto.java
+++ src/main/java/com/musa3team/devout/domain/store/dto/StoreRequestDto.java
@@ -2,6 +2,7 @@ package com.musa3team.devout.domain.store.dto;
 
 import com.musa3team.devout.common.constants.StoreCategory;
 import com.musa3team.devout.domain.store.valid.ValidTenMinuteInterval;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,15 +12,30 @@ import java.time.LocalTime;
 @Getter
 @AllArgsConstructor
 public class StoreRequestDto {
+    @Schema(description = "전화번호", example = "02-1234-1234")
     @Pattern(regexp = "^(02|0[3-6][1-5])-?\\d{3,4}-?\\d{4}$", message = "잘못된 전화번호 형식입니다.")
     private String telephone_number;
+
+    @Schema(description = "주소", example = "주소123-1234")
     private String address;
+
+    @Schema(description = "가게설명", example = "가게설명")
     private String contents;
+
+    @Schema(description = "가게이름", example = "가게이름")
     private String name;
+
+    @Schema(description = "open_time", example = "8:00")
     @ValidTenMinuteInterval
     private LocalTime open_time;
+
+    @Schema(description = "open_close", example = "21:00")
     @ValidTenMinuteInterval
     private LocalTime close_time;
+
+    @Schema(description = "최소주문금액", example = "10000")
     private Long minimum_price;
+
+    @Schema(description = "카테고리", example = "CHICKEN")
     private StoreCategory category;
 }
