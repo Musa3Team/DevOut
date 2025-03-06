bdccd2715bdb710741cfdf98eebdbfe88727af55
DestroTHS <osk3130@gmail.com>
2025-03-06 16:15:12 +0900

    feat : add

--- src/main/java/com/musa3team/devout/domain/order/dto/request/CreateOrderItemRequest.java
+++ src/main/java/com/musa3team/devout/domain/order/dto/request/CreateOrderItemRequest.java
@@ -1,8 +1,12 @@
 package com.musa3team.devout.domain.order.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateOrderItemRequest {
     private Long menuId;
     private int count;
