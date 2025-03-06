e58861d94f1e02e390265fe82d15185e65882259
AirIHL <inhak1122@gmail.com>
2025-03-06 19:33:19 +0900

    test: Add MenuRepositoryTest And MenuServiceTest

--- src/main/java/com/musa3team/devout/domain/menu/dto/CreateMenuRequestDto.java
+++ src/main/java/com/musa3team/devout/domain/menu/dto/CreateMenuRequestDto.java
@@ -4,11 +4,11 @@ import com.musa3team.devout.domain.menu.entity.MenuCategory;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
-@Setter
 @Getter
+@AllArgsConstructor
 public class CreateMenuRequestDto {
 
     @NotBlank(message = "메뉴 이름은 필수 입력 값입니다.")
@@ -22,5 +22,4 @@ public class CreateMenuRequestDto {
 
     @NotNull(message = "카테고리는 필수 입력 값입니다.")
     private MenuCategory category;
-
 }
--- src/main/java/com/musa3team/devout/domain/menu/dto/UpdateMenuRequestDto.java
+++ src/main/java/com/musa3team/devout/domain/menu/dto/UpdateMenuRequestDto.java
@@ -4,9 +4,11 @@ import com.musa3team.devout.domain.menu.entity.MenuCategory;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class UpdateMenuRequestDto {
 
     @NotBlank(message = "메뉴 이름은 필수 입력 값입니다.")
