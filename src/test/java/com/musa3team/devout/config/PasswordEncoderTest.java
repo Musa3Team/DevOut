d288745637d83e9bb5e1d3792a5349c5c37cf29e
minjonyyy <minjonyyy@naver.com>
2025-03-06 15:04:51 +0900

    feat : 테스트코드 작성

---
+++ src/test/java/com/musa3team/devout/config/PasswordEncoderTest.java
@@ -0,0 +1,31 @@
+package com.musa3team.devout.config;
+
+import com.musa3team.devout.common.config.PasswordEncoder;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(SpringExtension.class)
+public class PasswordEncoderTest {
+
+    @InjectMocks
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    void matches_메서드_정상작동(){
+        //given
+        String rawPassword = "password";
+        String encodedPassword = passwordEncoder.encode(rawPassword);
+
+        //when
+        boolean matches = passwordEncoder.matches(rawPassword, encodedPassword);
+
+        //then
+        assertTrue(matches);
+    }
+
+
+}
