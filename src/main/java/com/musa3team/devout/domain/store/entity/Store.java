6b67b5aa46843e49617bc4981c651f7132a451f7
songkongji <seungmin576@gmail.com>
2025-03-06 20:21:06 +0900

    refactor : Store Entity의 @Setter 삭제 Entity에는 Setter 사용을 지양해야 한다고 하여 @Setter를 삭제하며 그에 따른 service 코드를 수정했습니다.

--- src/main/java/com/musa3team/devout/domain/store/entity/Store.java
+++ src/main/java/com/musa3team/devout/domain/store/entity/Store.java
@@ -9,15 +9,19 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
+
+import static com.musa3team.devout.domain.store.valid.TenMinuteIntervalValidator.isValidTenMinuteInterval;
 
 @Entity
 @Getter
 @NoArgsConstructor
-@Setter
 public class Store extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -60,5 +64,40 @@ public class Store extends BaseEntity {
         this.minimumPrice = minimumPrice;
         this.category = category;
         this.member = member;
+    }
+
+    public void uddateStatus(StoreStatus status) {
+        this.status = status;
+    }
+
+    public void updateStore(StoreStatus status,String address, String telephoneNumber, StoreCategory category, String name, String contents, int minimumPrice, LocalTime openTime, LocalTime closeTime) {
+        if(status.equals(StoreStatus.SHUTDOWN))
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "폐업한 가게입니다.");
+
+        if (address != null && !address.isBlank()) this.address = address;
+
+        if (category != null) this.category = category;
+
+        if (name != null && !name.isBlank()) this.name = name;
+
+        if (contents != null && !contents.isBlank()) this.contents = contents;
+
+        if (minimumPrice > 0) this.minimumPrice = minimumPrice;
+
+        if (telephoneNumber != null && !telephoneNumber.isBlank()) {
+            if (Pattern.matches("^(02|0[3-6][1-5])-?\\d{3,4}-?\\d{4}$", telephoneNumber))
+                this.telephoneNumber = telephoneNumber;
+            else throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "전화번호 형식을 확인하세요");
+        }
+
+        if (openTime != null) {
+            if (isValidTenMinuteInterval(openTime)) this.openTime = openTime;
+            else throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "분은 10분 단위만 입력 가능합니다.");
+        }
+
+        if (closeTime != null) {
+            if (isValidTenMinuteInterval(closeTime)) this.closeTime = closeTime;
+            else throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "분은 10분 단위만 입력 가능합니다.");
+        }
     }
 }
